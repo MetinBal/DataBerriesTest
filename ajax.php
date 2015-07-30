50bce5cb0ce9d282bd64add9e6cdaa85b5d9d151
Metin Balci <davutmetinbalci@gmail.com>
2015-07-30 14:38:13 +0300

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -1,7 +1,7 @@
 <?php
 class ajaxValidate {
         function formValidate() {
-                //Put form elements into post variables (this is where you would sanitize your data)
+                
                 $nom_client = @$_POST['nom_client'];
 				$nom_entreprise = @$_POST['nom_entreprise'];
 				$adresse_numero_rue = @$_POST['adresse_numero_rue'];
@@ -10,16 +10,20 @@ class ajaxValidate {
 				$adresse_code_postal = @$_POST['adresse_code_postal'];
 				$adresse_ville = @$_POST['adresse_ville'];
 				$email_client = @$_POST['email_client'];
-                //Establish values that will be returned via ajax
+				
+                //return values
                 $return = array();
                 $return['msg'] = '';
                 $return['error'] = false;
-                //Begin form validation functionality
+				
+                // form validation functionality
                 if (!isset($nom_client) || empty($nom_client)){
                         $return['error'] = true;
                         $return['msg'] .= '<li>Nom du client pas renseigné.</li>';
                 }
-                //Begin form success functionality
+				
+                // Begin db  functionality
+				// connection
                 if ($return['error'] === false){
 						$mysqli = new mysqli("data.martibis.com", "user", "pwd", "database");
 						if ($mysqli->connect_errno) {
@@ -27,13 +31,13 @@ class ajaxValidate {
 							$return['msg'] .= '<li>Echec lors de la connexion à MySQL</li>';
 						}
                 }
+				//Insert query
 				if ($return['error'] === false){
 						
 						$sql = "INSERT INTO client(nom_client, nom_entreprise, adresse_numero_rue,adresse_nom_rue,adresse_code_postal,adresse_ville,email_client) 
 						VALUES('$nom_client', '$nom_entreprise', '$adresse_numero_rue', '$adresse_nom_rue', '$adresse_code_postal', '$adresse_ville', '$email_client')";
 						if(!$mysqli->query($sql))
 							{
-								//die('Error : ' . mysql_error());
 								$return['error'] = true;
 								$return['msg'] .= '<li>Echec lors de la requete  MySQL:' . mysql_error() . '</li>';
 							}
@@ -41,6 +45,7 @@ class ajaxValidate {
 				if ($return['error'] === false){
                         $return['msg'] = '<li>Enregistrement terminée</li>';
                 }
+				
                 //Return json encoded results
                 return json_encode($return);
         }
